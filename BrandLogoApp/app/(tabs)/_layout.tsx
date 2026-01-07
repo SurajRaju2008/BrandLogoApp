07b57fe202a7907564a47809a33993bb5196c8a7
SurajRaju2008 <surajraju@icloud.com>
2026-01-07 12:53:19 -0600

    tab nav initiated

---
+++ BrandLogoApp/app/(tabs)/_layout.tsx
@@ -0,0 +1,58 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import colors from "../styles/colors";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.tabs,
+        tabBarInactiveTintColor: "gray",
+        headerStyle: { backgroundColor: colors.primary },
+        headerShadowVisible: false,
+        headerTintColor: colors.tabs,
+        tabBarStyle: { backgroundColor: colors.primary, height: 70 },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerTitle: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "profile",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "people" : "people-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          headerTitle: "settings",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "school" : "school-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ BrandLogoApp/app/styles/colors.tsx
@@ -0,0 +1,16 @@
+
+export const colors = {
+  primary: '#0D4C92',   // Indian Blue from the logo
+  secondary: '#FFFFFF', // White for background and accents
+
+  // Optional shades for UI consistency
+  primaryLight: '#336BB3', // Lighter variant of Indian Blue
+  primaryDark: '#0A3970',  // Darker variant for hover states
+
+  // Text colors
+  textPrimary: '#0D4C92',  // Main text using primary color
+  textSecondary: '#555555', // Gray for secondary text
+
+  tabs: '#4A5679',
+};
+export default colors;
